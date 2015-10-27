945373561304c5e3a611ca60d0a9a0c99b44aff8
bradleybrentmeyer <bradleybrentmeyer@gmail.com>
2015-10-26 20:58:39 -0400

    Update HBaseTbl.java

--- HBaseTbl.java
+++ HBaseTbl.java
@@ -26,14 +26,14 @@ public class HBaseTbl {
 	
 	public HBaseTbl(String tblName) {
 		
-		 this.config = HBaseConfiguration.create();
-		 this.tblName = tblName;
+	    this.config = HBaseConfiguration.create();
+	    this.tblName = tblName;
 	}
   
 	public HTable getTable(String atable) throws IOException{
 		
-		  this.table = new HTable(this.config, atable);
-		  return this.table; 
+	    this.table = new HTable(this.config, atable);
+	    return this.table; 
 	}
 	
 	public void addKey(String akey) throws IOException{
@@ -43,46 +43,45 @@ public class HBaseTbl {
 	
 	public void addCol(String acolFam, String acol, String avalue) throws IOException{
 		
-		  this.p.add(Bytes.toBytes(acolFam), Bytes.toBytes(acol),Bytes.toBytes(avalue));
+	    this.p.add(Bytes.toBytes(acolFam), Bytes.toBytes(acol),Bytes.toBytes(avalue));
 	}
 	
 	public void addNumCol(String acolFam, String acol, Double avalue) throws IOException{
 		
-		  this.p.add(Bytes.toBytes(acolFam), Bytes.toBytes(acol),Bytes.toBytes(avalue));
+	    this.p.add(Bytes.toBytes(acolFam), Bytes.toBytes(acol),Bytes.toBytes(avalue));
 	}
 	
 	public void putRow() throws IOException{
 		
-		  this.table.put(p);
-			
+	    this.table.put(p);
 	}
 	
 	public ResultScanner scan() throws IOException{
 
-		Scan scan = new Scan();
-		ResultScanner result = this.table.getScanner(scan);
-		return result;
+	    Scan scan = new Scan();
+	    ResultScanner result = this.table.getScanner(scan);
+	    return result;
 	}
 	
 	public int getRowCount()throws IOException{
-		
-		int i = 0;
-		ResultScanner rs = this.scan(); 
-		for (Result r: rs){
-			i++;	
-		}
-		return i;	
+     		
+	     int i = 0;
+	     ResultScanner rs = this.scan(); 
+	     for (Result r: rs){
+	         i++;	
+	     }
+	     return i;	
 	}
-		
+	
 	public ArrayList<String> decodeScanner(ResultScanner scanner, String colFam, String col){
-		
-		ArrayList<String> results = new ArrayList<String>();
-		int i = 0;
-		for (Result result: scanner) {
-            // decode bytes to sent
-			results.add(Bytes.toString(result.getValue(toBytes(colFam), toBytes(col))));
-			i++; 
-		}
-		return results;
+	
+	    ArrayList<String> results = new ArrayList<String>();
+	    int i = 0;
+	    for (Result result: scanner) {
+                // decode bytes to sent
+	        results.add(Bytes.toString(result.getValue(toBytes(colFam), toBytes(col))));
+	        i++; 
+	    }
+	    return results;
 	}
 }
